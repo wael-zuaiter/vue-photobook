3b4609f2347f13887f51096ffe5a1e749d75db70
Wael Zoaiter <wa95el@hotmail.com>
2020-09-26 00:28:07 +0300

    Downgrade to vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div id="app">
+  <amplify-authenticator v-if="authState !== 'signedin'"></amplify-authenticator>
+  <div v-if="authState === 'signedin' && user">
+    <amplify-sign-out></amplify-sign-out>
+    <div>Hello, {{user.username}}</div>
   </div>
-  <router-view/>
+</div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
 }
 </style>
+
+<script>
+import {
+  onAuthUIStateChange
+} from '@aws-amplify/ui-components'
+
+export default {
+  name: 'AuthStateApp',
+  created() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState
+      this.user = authData
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined
+    }
+  },
+  beforeDestroy() {
+    return onAuthUIStateChange
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import '@aws-amplify/ui-vue'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import './assets/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app');
+Amplify.configure(awsconfig)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
